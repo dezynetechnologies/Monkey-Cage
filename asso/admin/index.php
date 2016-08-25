11e55435c094aa4efc2b05cce4dfa80a311aa04c
Saurav Kumar <saurav@dezyne.in>
2016-08-25 17:55:51 +0530

    Added the admin panel

---
+++ asso/admin/index.php
@@ -0,0 +1,409 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta charset="utf-8" />
+		<title>Monkey_Cage- Admin</title>
+
+		<meta name="description" content="overview &amp; stats" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+		<!-- bootstrap & fontawesome -->
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="assets/font-awesome/4.5.0/css/font-awesome.min.css" />
+
+		<!-- page specific plugin styles -->
+
+		<!-- text fonts -->
+		<link rel="stylesheet" href="assets/css/fonts.googleapis.com.css" />
+
+		<!-- ace styles -->
+		<link rel="stylesheet" href="assets/css/ace.min.css" class="ace-main-stylesheet" id="main-ace-style" />
+
+		<!--[if lte IE 9]>
+			<link rel="stylesheet" href="assets/css/ace-part2.min.css" class="ace-main-stylesheet" />
+		<![endif]-->
+		<link rel="stylesheet" href="assets/css/ace-skins.min.css" />
+		<link rel="stylesheet" href="assets/css/ace-rtl.min.css" />
+        <link rel="stylesheet" href="admin.css" />
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!--[if lte IE 9]>
+		  <link rel="stylesheet" href="assets/css/ace-ie.min.css" />
+		<![endif]-->
+
+		<!-- inline styles related to this page -->
+
+		<!-- ace settings handler -->
+		
+
+		<!-- HTML5shiv and Respond.js for IE8 to support HTML5 elements and media queries -->
+
+		<!--[if lte IE 8]>
+		<script src="assets/js/html5shiv.min.js"></script>
+		<script src="assets/js/respond.min.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+		<div class="container">
+  <div class="jumbotron">
+    <h1>Monkey-Cage</h1> 
+    <p>Admin Panel</p> 
+  </div>
+  <div class="panel panel-default">
+  <div class="panel-body">
+  	<h2  style="color:red;" >Pricing</h2>
+
+  <div class="panel-body">	
+
+  	<form method="post" action="" >
+<div class="col-lg-3">
+
+<h2 class="top_head" >Erwachsene</h2>
+
+<p><br></p>
+
+<div class="table-responsive" >
+	 <table class="table">
+
+	<tr>
+		<th>1 Day :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="A1day"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+   <tr>
+		<th>11er :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="A1month"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+   <tr>
+		<th>3-Monate :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="A3month"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+   <tr>
+		<th>Halbjahr :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="A6month"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+   <tr>
+		<th>Jahr :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="Ayear"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+</table>	
+	
+</div>
+</div>
+<div class="col-lg-3">
+	<h2 class="top_head" >Schüler</h2>
+
+<p><br></p>
+
+<div class="table-responsive" >
+	 <table class="table">
+
+	<tr>
+		<th>1 Day :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="S1day"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+   <tr>
+		<th>11er :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="S1month"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+   <tr>
+		<th>3-Monate :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="S3month"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+   <tr>
+		<th>Halbjahr :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="S6month"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+   <tr>
+		<th>Jahr :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="Syear"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+</table>	
+	
+</div>
+</div>
+<div class="col-lg-3">
+		<h2 class="top_head" >Kinder</h2>
+
+<p><br></p>
+
+<div class="table-responsive" >
+	 <table class="table">
+
+	<tr>
+		<th>1 Day :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="K1day"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+   <tr>
+		<th>11er :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="K1month"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+   <tr>
+		<th>3-Monate :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="K3month"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+   <tr>
+		<th>Halbjahr :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="K6month"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+   <tr>
+		<th>Jahr :</th>
+	</tr>
+
+
+	<tr>
+	<td> <input type="text" name="Kyear"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+</table>	
+	
+</div>
+</div>
+<div class="col-lg-3">
+	
+		<h2 class="top_head" >Familienkarte</h2>
+
+<p><br></p>
+
+<div class="table-responsive" >
+	 <table class="table">
+
+	<tr>
+		<th>1 Day :</th>
+	</tr>
+
+	<tr>
+	<td> <input type="text" name="F1day"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+</table>	
+	
+<p><br></p>
+
+<h2 class="top_head" >Kleinkinder</h2>
+
+<p><br></p>
+
+<div class="table-responsive" >
+	 <table class="table">
+
+	<tr>
+		<th>1 Day :</th>
+	</tr>
+
+	<tr>
+	<td> <input type="text" name="KK1day"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+	
+
+</table>	
+	
+</div>
+</div>
+</div>
+
+<div class="col-lg-12">
+	<h2 style="color:red;">Leihequipment</h2>
+</div>
+
+<div class="col-lg-6">
+
+
+	
+		<h2 class="top_head"  >Erwachsene</h2>
+
+<p><br></p>
+
+<div class="table-responsive" >
+	 <table class="table">
+
+	<tr>
+		<th>Leihschuhe</th>
+	</tr>
+
+	<tr>
+	<td> <input type="text" name="EEL"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+<tr>
+		<th>Chalkbags</th>
+	</tr>
+
+	<tr>
+	<td> <input type="text" name="EEC"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+   <tr>
+		<th>Bürsten</th>
+	</tr>
+
+	<tr>
+	<td> <input type="text" name="EEB"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+
+	
+
+</table>	
+	
+</div>
+</div>
+
+<div class="col-lg-6">
+
+	
+		<h2 class="top_head" >Kinder</h2>
+
+<p><br></p>
+
+<div class="table-responsive" >
+	 <table class="table">
+
+	<tr>
+		<th>Leihschuhe</th>
+	</tr>
+
+	<tr>
+	<td> <input type="text" name="EKL"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+<tr>
+		<th>Chalkbags</th>
+	</tr>
+
+	<tr>
+	<td> <input type="text" name="EKC"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+   <tr>
+		<th>Bürsten</th>
+	</tr>
+
+	<tr>
+	<td> <input type="text" name="EKB"  placeholder="Eingeben Preis" ></input> </td>
+   </tr>
+
+	
+
+</table>	
+	
+</div>
+</div>
+
+</div>
+<div class="col-lg-12" style="text-align: center;">
+<input type="submit"  name="update" value="Einreichen" class="btn btn-danger"> </input>
+
+
+
+
+
+
+
+
+</div>
+
+</form>
+</div>
+  
+</div>
+  
+</div>
+</div>
+
+
+
+
+<?php
+
+
+if (isset($_POST['update'])) {
+	
+
+
+	$A1day = $_POST['A1day'];
+	$A1month = $_POST['A1month'];
+
+
+	echo $A1day . $A1month ;
+
+
+
+}
+
+
+?>
+</body>
+</html>
