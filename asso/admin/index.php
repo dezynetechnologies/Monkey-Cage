4b639438fb27c9f42fd4057de62a88b6818d9771
Saurav Kumar <saurav@dezyne.in>
2016-08-30 17:58:38 +0530

    Admin panel backend part1

--- asso/admin/index.php
+++ asso/admin/index.php
@@ -61,22 +61,71 @@ else{
 		<script src="assets/js/respond.min.js"></script>
 		<![endif]-->
 
-    <script>
-function myFunction() {
-    var x, text;
+    
+<!--<script>
+function validateForm() {
+    var a1d = document.forms["myForm"]["A1day"].value;
+    var a1m= document.forms["myForm"]["A1month"].value;
+    var a3m= document.forms["myForm"]["A3month"].value;
+    var a6m= document.forms["myForm"]["A6month"].value;
+    var s1d= document.forms["myForm"]["S1day"].value;
+    var s1m= document.forms["myForm"]["S1month"].value;
+    var s3m= document.forms["myForm"]["S3month"].value;
+    var s6m= document.forms["myForm"]["S6month"].value;
+    var sy= document.forms["myForm"]["Syear"].value;
+    var k1d= document.forms["myForm"]["K1day"].value;
+    var k1m= document.forms["myForm"]["K1month"].value;
+    var k3m= document.forms["myForm"]["K3month"].value;
+    var k6m= document.forms["myForm"]["K6month"].value;
+    var ky= document.forms["myForm"]["Kyear"].value;
+    var f1d= document.forms["myForm"]["F1day"].value;
+    var kk1d= document.forms["myForm"]["KK1day"].value;
 
-    // Get the value of the input field with id="numb"
-    x = document.getElementById("numb").value;
 
-    // If x is Not a Number or less than one or greater than 10
-    if (isNaN(x) || x < 1 || x > 10) {
-        text = "Input not valid";
-    } else {
-        text = " ";
+
+    if (a1d == null || a1d == "" || a1m == null || a1m == "" ||  a3m == null || a3m == "" ||  a6m == null ||a6m == "" ||  ay == null ||ay == ""
+    ||  s1d == null || s1d == "" || s1m == null || s1m == "" ||  s3m == null || s3m == "" ||  s6m == null ||s6m == "" ||  sy == null ||sy == ""
+    ||  k1d == null || k1d == "" || k1m == null || k1m == "" ||  k3m == null || k3m == "" ||  k6m == null ||k6m == "" ||  ky == null ||ky == ""
+    ||  f1d == null || f1d == "" || kk1d == null || kk1d == "") {
+
+        text="Only Integers must be filled";
+
+        document.getElementById("demo1").innerHTML = text;       
+        document.getElementById("demo2").innerHTML = text;
+        document.getElementById("demo3").innerHTML = text;
+        document.getElementById("demo4").innerHTML = text;
+        document.getElementById("demo5").innerHTML = text;
+        document.getElementById("demo6").innerHTML = text;
+        document.getElementById("demo7").innerHTML = text;
+        document.getElementById("demo8").innerHTML = text;
+        document.getElementById("demo9").innerHTML = text;
+        document.getElementById("demo10").innerHTML = text;
+        document.getElementById("demo11").innerHTML = text;
+        document.getElementById("demo12").innerHTML = text;
+        document.getElementById("demo13").innerHTML = text;
+        document.getElementById("demo14").innerHTML = text;
+        document.getElementById("demo15").innerHTML = text;
+        document.getElementById("demo16").innerHTML = text;
+        document.getElementById("demo17").innerHTML = text;
+
+
+
+
+return false;
     }
-    document.getElementById("demo").innerHTML = text;
+
 }
+</script>-->
 </script>
+
+ <script language="javascript">
+    function enableDisable(bEnable, textBoxID, textBoxID1)
+    {
+         document.getElementById(textBoxID).disabled = bEnable
+         document.getElementById(textBoxID1).disabled = bEnable
+    }
+</script>
+   
 	</head>
 
 
@@ -97,7 +146,7 @@ function myFunction() {
 
   <div class="panel-body">	
 
-  	<form  name="myform" method="post"  action="" enctype="multipart/form-data" >
+  	<form  name="myForm"   action=""   method="post" >
 <h2  style="color:red;" >Timings</h2>
   		<div class="col-lg-12">
           <div class="table-responsive">
@@ -117,7 +166,7 @@ function myFunction() {
 	<tr>
 	<td>
 <label>Von: </label>
-<select name="SATo">
+<select name="SATo" id="sato" >
 		<?php
 		for ($i=0; $i < 24 ; $i++) { 
 			
@@ -128,7 +177,7 @@ function myFunction() {
 </select>
 <br>-<br>
 <label>Nach: </label>
-<select name="SATc">
+<select name="SATc" id="satc" >
 		<?php
 		for ($i=0; $i < 24 ; $i++) { 
 			
@@ -136,13 +185,13 @@ function myFunction() {
   echo "<option>$i : 00  </option> ";
 }
   ?> 
-</select><br><br><input type="checkbox" name="satclo">Closed<br></td>
+</select><br><br><input type="checkbox" name="satclo" id="checkBox" onclick="enableDisable(this.checked, 'sato','satc')">Closed<br></td>
 
 
 
 	<td>
     <label>Von: </label>
-<select name="SUNo">
+<select name="SUNo" id="suno">
 		<?php
 		for ($i=0; $i < 24 ; $i++) { 
 			
@@ -153,7 +202,7 @@ function myFunction() {
 </select>
 <br>-<br>
 <label>Nach: </label>
-<select name="SUNc">
+<select name="SUNc" id="sunc">
 		<?php
 		for ($i=0; $i < 24 ; $i++) { 
 			
@@ -161,11 +210,11 @@ function myFunction() {
   echo "<option>$i : 00  </option> ";
 }
   ?> 
-</select><br><br><input type="checkbox" name="sunclo">Closed<br></td>
+</select><br><br><input type="checkbox" name="sunclo" id="checkBox" onclick="enableDisable(this.checked, 'suno','sunc')">Closed<br></td>
 	<td>
     <label>Von: </label>
 
-<select name="MONo">
+<select name="MONo" id="mono">
 		<?php
 		for ($i=0; $i < 24 ; $i++) { 
 			
@@ -176,7 +225,7 @@ function myFunction() {
 </select>
 <br>-<br>
 <label>Nach: </label>
-<select name="MONc">
+<select name="MONc" id="monc">
 		<?php
 		for ($i=0; $i < 24 ; $i++) { 
 			
@@ -184,11 +233,11 @@ function myFunction() {
   echo "<option>$i : 00  </option> ";
 }
   ?> 
-</select><br><br><input type="checkbox" name="monclo">Closed<br></td>
+</select><br><br><input type="checkbox" name="monclo" id="checkBox" onclick="enableDisable(this.checked, 'mono','monc')">Closed<br></td>
 	<td>
     <label>Von: </label>
 
-<select name="TUEo">
+<select name="TUEo" id="tueo">
 		<?php
 		for ($i=0; $i < 24 ; $i++) { 
 			
@@ -199,7 +248,7 @@ function myFunction() {
 </select>
 <br>-<br>
 <label>Nach: </label>
-<select name="TUEc">
+<select name="TUEc" id="tuec">
 		<?php
 		for ($i=0; $i < 24 ; $i++) { 
 			
@@ -207,22 +256,22 @@ function myFunction() {
   echo "<option>$i : 00  </option> ";
 }
   ?> 
-</select><br><br><input type="checkbox" name="tueclo">Closed<br></td></input></td>
+</select><br><br><input type="checkbox" name="tueclo" id="checkBox" onclick="enableDisable(this.checked, 'tueo','tuec')">Closed<br></td></input></td>
 	<td>
     <label>Von: </label>
 
-<select name="WEDo">
+<select name="WEDo" id="wedo">
 		<?php
 		for ($i=0; $i < 24 ; $i++) { 
 			
 		
   echo "<option>$i : 00  </option> ";
-}
+} 
   ?> 
 </select>
 <br>-<br>
 <label>Nach: </label>
-<select name="WEDc">
+<select name="WEDc" id="wedc">
 		<?php
 		for ($i=0; $i < 24 ; $i++) { 
 			
@@ -230,11 +279,11 @@ function myFunction() {
   echo "<option>$i : 00  </option> ";
 }
   ?> 
-</select><br><br><input type="checkbox" name="wedclo">Closed<br></td>
+</select><br><br><input type="checkbox" name="wedclo" id="checkBox" onclick="enableDisable(this.checked, 'wedo','wedc')">Closed<br></td>
 	<td>
     <label>Von: </label>
 
-<select name="THUo">
+<select name="THUo" id="thuo">
 		<?php
 		for ($i=0; $i < 24 ; $i++) { 
 			
@@ -245,7 +294,7 @@ function myFunction() {
 </select>
 <br>-<br>
 <label>Nach: </label>
-<select name="THUc">
+<select name="THUc" id="thuc" >
 		<?php
 		for ($i=0; $i < 24 ; $i++) { 
 			
@@ -253,11 +302,11 @@ function myFunction() {
   echo "<option>$i : 00  </option> ";
 }
   ?> 
-</select><br><br><input type="checkbox" name="thuclo">Closed<br></td>
+</select><br><br><input type="checkbox" name="thuclo" id="checkBox" onclick="enableDisable(this.checked, 'thuo','thuc')">Closed<br></td>
 	<td>
     <label>Von: </label>
 
-<select name="FRIo">
+<select name="FRIo" id="frio">
 		<?php
 		for ($i=0; $i < 24 ; $i++) { 
 			
@@ -268,7 +317,7 @@ function myFunction() {
 </select>
 <br>-<br>
 <label>Nach: </label>
-<select name="FRIc">
+<select name="FRIc" id="fric">
 		<?php
 		for ($i=0; $i < 24 ; $i++) { 
 			
@@ -276,7 +325,7 @@ function myFunction() {
   echo "<option>$i : 00  </option> ";
 }
   ?> 
-</select><br><br><input type="checkbox" name="friclo">Closed<br></td>
+</select><br><br><input type="checkbox" name="friclo" id="checkBox" onclick="enableDisable(this.checked, 'frio','fric')">Closed<br></td>
    </tr>
   
 
@@ -562,7 +611,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" id="numb" name="A1day"  placeholder="Eingeben Preis"  value="xyz"> <p id="demo"></p> </input> </td> 
+	<td><input type="number" step=0.01 name="A1day" placeholder="Eingeben Preis"> </input> <p id="demo1" ></p> </td> 
 
    </tr>
 
@@ -572,7 +621,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" name="A1month" id="numb"  placeholder="Eingeben Preis" > <p id="demo"> </input></td>
+	<td> <input type="number" step=0.01 name="A1month" placeholder="Eingeben Preis" >  </input><p id="demo2" ></p></td>
    </tr>
 
    <tr>
@@ -581,7 +630,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" name="A3month"  placeholder="Eingeben Preis" ></input><span class="error" style ="color:red;">* <?php echo $A3montherr ;?></span></td>
+	<td> <input type="number" step=0.01 name="A3month"  placeholder="Eingeben Preis" ></input><p id="demo3" ></p></td>
    </tr>
 
    <tr>
@@ -590,7 +639,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" name="A6month"  placeholder="Eingeben Preis" ></input> <span class="error" style ="color:red;">* <?php echo $A6montherr ;?></span> </td>
+	<td> <input type="number" step=0.01 name="A6month"  placeholder="Eingeben Preis" ></input><p id="demo4" ></p>  </td>
    </tr>
 
    <tr>
@@ -599,7 +648,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" name="Ayear"  placeholder="Eingeben Preis" ></input>   <span class="error" style ="color:red;">* <?php echo $Ayearerr ;?></span></td>
+	<td> <input type="number" step=0.01 name="Ayear"  placeholder="Eingeben Preis" ></input><p id="demo5" ></p>  </td>
    </tr>
 
 </table>	
@@ -620,7 +669,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" name="S1day"  placeholder="Eingeben Preis" ></input>  <span class="error" style ="color:red;">* <?php echo $S1dayerr ;?></span> </td>
+	<td> <input type="number" step=0.01 name="S1day"  placeholder="Eingeben Preis" ></input><p id="demo6" ></p>  </td>
    </tr>
 
    <tr>
@@ -629,7 +678,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" name="S1month"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="S1month"  placeholder="Eingeben Preis" ></input><p id="demo7" ></p> </td>
    </tr>
 
    <tr>
@@ -638,7 +687,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" name="S3month"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="S3month"  placeholder="Eingeben Preis" ></input> <p id="demo8" ></p></td>
    </tr>
 
    <tr>
@@ -647,7 +696,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" name="S6month"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="S6month"  placeholder="Eingeben Preis" ></input> <p id="demo9" ></p></td>
    </tr>
 
    <tr>
@@ -656,7 +705,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" name="Syear"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="Syear"  placeholder="Eingeben Preis" ></input><p id="demo10" ></p> </td>
    </tr>
 
 </table>	
@@ -677,7 +726,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" name="K1day"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="K1day"  placeholder="Eingeben Preis" ></input><p id="demo11" ></p> </td>
    </tr>
 
    <tr>
@@ -686,7 +735,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" name="K1month"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="K1month"  placeholder="Eingeben Preis" ></input><p id="demo12" ></p> </td>
    </tr>
 
    <tr>
@@ -695,7 +744,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" name="K3month"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="K3month"  placeholder="Eingeben Preis" ></input><p id="demo13" ></p> </td>
    </tr>
 
    <tr>
@@ -704,7 +753,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" name="K6month"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="K6month"  placeholder="Eingeben Preis" ></input><p id="demo14" ></p> </td>
    </tr>
 
    <tr>
@@ -713,7 +762,7 @@ function myFunction() {
 
 
 	<tr>
-	<td> <input type="text" name="Kyear"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="Kyear"  placeholder="Eingeben Preis" ></input><p id="demo15" ></p> </td>
    </tr>
 
 </table>	
@@ -734,7 +783,7 @@ function myFunction() {
 	</tr>
 
 	<tr>
-	<td> <input type="text" name="F1day"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="F1day"  placeholder="Eingeben Preis" ></input><p id="demo16" ></p> </td>
    </tr>
 
 </table>	
@@ -753,7 +802,7 @@ function myFunction() {
 	</tr>
 
 	<tr>
-	<td> <input type="text" name="KK1day"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="KK1day"  placeholder="Eingeben Preis" ></input><p id="demo17" ></p> </td>
    </tr>
 
 	
@@ -784,14 +833,14 @@ function myFunction() {
 	</tr>
 
 	<tr>
-	<td> <input type="text" name="EEL"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="EEL"  placeholder="Eingeben Preis" ></input><p id="demo18" ></p> </td>
    </tr>
 <tr>
 		<th>Chalkbags</th>
 	</tr>
 
 	<tr>
-	<td> <input type="text" name="EEC"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="EEC"  placeholder="Eingeben Preis" ></input><p id="demo19" ></p> </td>
    </tr>
 
    <tr>
@@ -799,7 +848,7 @@ function myFunction() {
 	</tr>
 
 	<tr>
-	<td> <input type="text" name="EEB"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="EEB"  placeholder="Eingeben Preis" ></input><p id="demo20" ></p> </td>
    </tr>
 
 
@@ -825,7 +874,7 @@ function myFunction() {
 	</tr>
 
 	<tr>
-	<td> <input type="text" name="EKL"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="EKL"  placeholder="Eingeben Preis" ></input> <p id="demo21" ></p></td>
    </tr>
 
 <tr>
@@ -833,7 +882,7 @@ function myFunction() {
 	</tr>
 
 	<tr>
-	<td> <input type="text" name="EKC"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="EKC"  placeholder="Eingeben Preis" ></input><p id="demo22" ></p> </td>
    </tr>
 
    <tr>
@@ -841,7 +890,7 @@ function myFunction() {
 	</tr>
 
 	<tr>
-	<td> <input type="text" name="EKB"  placeholder="Eingeben Preis" ></input> </td>
+	<td> <input type="number" step=0.01 name="EKB"  placeholder="Eingeben Preis" ></input><p id="demo23" ></p> </td>
    </tr>
 
 	
@@ -853,18 +902,10 @@ function myFunction() {
 
 </div>
 <div class="col-lg-12" style="text-align: center;">
-<button name="update"  type="button" onclick="myFunction()" value="Einreichen" class="btn btn-danger">Submit </button>
-<button name="update"  type="submit" value="Einreichen" class="btn btn-danger">Submit </button>
 
-
+<input name="update"  type="submit" value="Einreichen" >
  <?php
-$A1dayerr =  $A1montherr = $A3montherr = $A6montherr = $Ayearerr = " ";
-$S1dayerr = $S1montherr= $S3montherr= $S6montherr= $Syearerr=" ";
-$K1dayerr= $K1montherr = $K3montherr= $K6montherr= $Kyearerr=" ";
-$F1dayerr =" ";
-$KK1dayerr = " ";
-$EELerr = $EECerr = $EEBerr=" ";
-$EKLerr = $EKCerr = $EKBerr = " ";
+
 
 if (isset($_POST['update'])) {
   
@@ -873,167 +914,13 @@ if (isset($_POST['update'])) {
   $A1day = $_POST['A1day'];  
     echo $A1day;
   
+$A1month = $_POST['A1month'];
 
-  $A1month = $_POST['A1month'];
-   if (!preg_match('/^[0-9 ].*$/', $A1month)) {
+echo $A1month;
 
-$A1montherr = "error";
-
-    
-  }
-  $A3month = $_POST['A3month'];
-   if (!preg_match('/^[0-9 ].*$/', $A3month)) {
-
-$A3montherr = "error";
-
-    
-  }
-  $A6month = $_POST['A6month'];
-   if (!preg_match('/^[0-9 ].*$/', $A6month)) {
-
-$A6montherr = "error";
-
-    
-  }
-  $Ayear = $_POST['Ayear'];
-   if (!preg_match('/^[0-9 ].*$/', $Ayear)) {
-
-$Ayearerr = "error";
-
-    
+ 
   }
 
-  $S1day = $_POST['S1day'];
-  if (!preg_match('/^[0-9 ].*$/', $S1day)) {
-
-$S1dayerr = "error";
-
-    
-  }
-  $S1month = $_POST['S1month'];
-    if (!preg_match('/^[0-9 ].*$/', $S1month)) {
-
-$S1montherr = "error";
-
-    
-  }
-
-  $S3month = $_POST['S3month'];
-    if (!preg_match('/^[0-9 ].*$/', $S3month)) {
-
-$S3montherr = "error";
-
-    
-  }
-  $S6month = $_POST['S6month'];
-    if (!preg_match('/^[0-9 ].*$/', $S6month)) {
-
-$S6montherr = "error";
-
-    
-  }
-  $Syear = $_POST['Syear'];
-    if (!preg_match('/^[0-9 ].*$/', $Syear)) {
-
-$Syearerr = "error";
-
-    
-  }
-
-  $K1day = $_POST['K1day'];
-    if (!preg_match('/^[0-9 ].*$/', $K1day)) {
-
-$K1dayerr = "error";
-
-    
-  }
-  $K1month = $_POST['K1month'];
-    if (!preg_match('/^[0-9].*$/', $K1month)) {
-
-$K1montherr = "error";
-
-    
-  }
-  $K3month = $_POST['K3month'];
-    if (!preg_match('/^[0-9].*$/', $K3month)) {
-
-$K3montherr = "error";
-
-    
-  }
-  $K6month = $_POST['K6month'];
-    if (!preg_match('/^[0-9].*$/', $A1day)) {
-
-$K6montherr = "error";
-
-    
-  }
-  $Kyear = $_POST['Kyear']; 
-  if (!preg_match('/^[0-9].*$/', $Kyear)) {
-
-$Kyearerr = "error";
-
-    
-  }
-
-  $F1day = $_POST['F1day'];
-  if (!preg_match('/^[0-9].*$/', $F1day )) {
-
-$F1dayerr = "error";
-
-    
-  }
-  $KK1day = $_POST['KK1day'];
-  if (!preg_match('/^[0-9].*$/', $KK1day )) {
-
-$KK1dayerr = "error";
-
-    
-  }
-
-  $EEL = $_POST['EEL'];
-  if (!preg_match('/^[0-9].*$/', $EEL)) {
-
-$EELerr = "error";
-   
-  }
-  $EEC = $_POST['EEC'];
-  if (!preg_match('/^[0-9].*$/', $EEC)) {
-
-$EECerr = "error";
-
-    
-  }
-  $EEB = $_POST['EEB'];
-  if (!preg_match('/^[0-9].*$/', $EEB)) {
-
-$EEBerr = "error";
-
-    
-  }
-
-  $EKL = $_POST['EKL'];
-  if (!preg_match('/^[0-9 ].*$/', $EKL)) {
-
-$EKLerr = "error";
-
-    
-  }
-  $EKC = $_POST['EKC'];
-  if (!preg_match('/^[0-9].*$/', $EKC)) {
-
-$EKCerr = "error";
-
-    
-  }
-  $EKB = $_POST['EKB'];
-  if (!preg_match('/^[0-9].*$/', $EKB)) {
-
-$EKBerr = "error";
-
-    
-  }
-}
 
 
 ?>
